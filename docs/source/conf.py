4dbd8c33b8810eecff1cade275e2ce386a681f2d
Illustar0 <me@illustar0.com>
2024-12-22 21:53:55 +0800

    chore: bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../zzupy'))
 project = 'ZZU.Py'
 copyright = '2024, Illustar0'
 author = 'Illustar0'
-release = '0.2.2'
+release = '0.2.3'
 autoclass_content = 'both'
 
 
