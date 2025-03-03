da19688c8c4dec44aa10b4b22eebf4de9ae570ab
Illustar0 <me@illustar0.com>
2025-03-03 20:37:02 +0800

    feat: more detailed exceptions

--- zzupy/exception.py
+++ zzupy/exception.py
@@ -1,2 +1,27 @@
-class LoginException(Exception):
+class ZZUPyException(Exception):
+    """ZZUPy项目的基类异常"""
+
+    pass
+
+
+class LoginException(ZZUPyException):
+    """登录相关异常"""
+
+    pass
+
+
+class DefaultRoomException(ZZUPyException):
+    """默认房间相关异常"""
+
+    pass
+
+
+class ECardTokenException(ZZUPyException):
+    """校园卡 Token 相关异常"""
+
+    pass
+
+class PermissionException(ZZUPyException):
+    """校园卡 Token 相关异常"""
+
     pass
